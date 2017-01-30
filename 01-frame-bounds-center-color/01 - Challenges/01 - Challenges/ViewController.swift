6a72bbe0ce280b688d8bb2ccfa4e911d1c233ace
Alexander Dejeu <alexdejeu@gmail.com>
2017-01-29 17:57:14 -0800

    Challenge 4 finished

--- 01-frame-bounds-center-color/01 - Challenges/01 - Challenges/ViewController.swift
+++ 01-frame-bounds-center-color/01 - Challenges/01 - Challenges/ViewController.swift
@@ -16,7 +16,8 @@ class ViewController: UIViewController {
         
 //        challengeOne() // Red Box
 //        challengeTwo() // Chessboard
-        challengeThree() // Color grid
+//        challengeThree() // Color grid
+//        challengeFour() //concentricSquares
     }
     
     
@@ -47,12 +48,19 @@ class ViewController: UIViewController {
             let hue = 1.0 / (Double)(count) * Double(i)
             smallerLayer.backgroundColor = UIColor(hue: CGFloat(hue), saturation: 1, brightness: 1, alpha: 1).cgColor
             
-            
-            
             view.layer.addSublayer(smallerLayer)
         }
-        
         self.view.addSubview(view)
+    }
+    
+    func challengeFour(){
+        for i in 0 ..< 10 {
+            let layer = CALayer()
+            layer.frame = self.view.frame.insetBy(dx: 20 * CGFloat(i), dy: 20 * CGFloat(i))
+            
+            layer.backgroundColor = UIColor(hue: 0.5, saturation: 1.0 / CGFloat(i), brightness: 1.0, alpha: 1.0).cgColor
+            self.view.layer.addSublayer(layer)
+        }
     }
     
 }
