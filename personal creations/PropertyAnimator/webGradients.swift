997b450e1e41a50489f0df518ca8a3b1b9fed8b1
Alexander Dejeu <alexdejeu@gmail.com>
2017-02-15 22:36:45 -0800

    added the ground work for the gradients open source

---
+++ personal creations/PropertyAnimator/webGradients.swift
@@ -0,0 +1,23 @@
+//
+//  webGradients.swift
+//  PropertyAnimator
+//
+//  Created by Alexander Dejeu on 2/14/17.
+//  Copyright © 2017 Alexander Dejeu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+/*
+ Inspired by https://github.com/itmeo/webgradients
+ At time of creation there was only CSS3, .sketch and .PSD files.  I wondered why only web devs could reap
+ benifits of this spectacular project!  I ported all of the color gradients over to Swift and added some of my
+ own functions that I beilve will be useful for others as well.
+ */
+
+class gradients{
+  // 001 Warm Flame
+  static let warmFlameS1 = UIColor.red
+  static let warmFlameS2 = UIColor.yellow
+  static let warmFlame : [UIColor] = [warmFlameS1, warmFlameS2]
+}
